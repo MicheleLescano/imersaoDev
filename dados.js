7f80dbeaac95797e491d286563a1445ac7c419ec
Michele Lescano <104702362+MicheleLescano@users.noreply.github.com>
2024-09-06 20:25:20 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,48 @@
+
+
+let dados = [
+    {
+        titulo: "Igreja Matriz",
+        descricao: "Igreja Matriz, é o nome mais conhecido da igreja Nossa senhora da conceição pelos habitantes da cidade de Viamão, no momento com 255 anos.",
+        link: "https://pt.wikipedia.org/wiki/Igreja_Matriz_de_Nossa_Senhora_da_Concei%C3%A7%C3%A3o_(Viam%C3%A3o)",
+        tags: "igreja matriz centro de viamão viamao ",
+    },
+    {
+       titulo:" Eco Resort Vila Ventura",
+       descricao:"Viamão possui diferentes alternativas de hospedagem em seu Município, e o resort Vila Ventura é um deles,Conforto, privacidade e muito carinho com os detalhes. Estrutura completa com 8 espaços diferentes, que vão de 72m2 até 1000m2, capazes de receber eventos de diversos formatos.",
+       link:"https://www.vilaventura.com.br/",
+       tags:" lazer Eco Resort Vila Ventura vila Ventura resort eco turismo hospedagem",
+    },
+    {
+        titulo:"QUINTA DA ESTÂNCIA",
+        descricao:"QUINTA DA ESTÂNCIA - fazenda referência internacional em Turismo Ecológico, Em uma área de 125 hectares, os visitantes usufruem de uma infraestrutura completa, o que permite a realização de mais de 80 opções de atividades pedagógicas, ecológicas, de lazer e dinâmicas vivenciais.",
+        link:"https://www.quintadaestancia.com.br/infraestrutura",
+        tags:"fazenda estancia turismo ecologico lazer",
+     },
+     {
+        titulo:"",
+        descricao:"O Automóvel Clube do Rio Grande do Sul, proprietário do Autódromo de Tarumã é uma entidade sem fins lucrativos e tem como principal missão o fomento do automobilismo através de seus associados. Fundado no dia 17 de junho de 1953, o Clube surgiu de uma outra agremiação mais antiga, a Associação Rio Grandense de Volantes, entidade que foi fundada em 1949 com o intuito de organizar corridas por todo o estado. ",
+        link:"https://www.autodromodetaruma.com.br/",
+        tags:"corrida automovel racha tarumã competição",
+     },
+     {
+        titulo:"Lago Tarumã",
+        descricao:"O Lago situado na cidade de Viamão,ponto de encontro na cidade que costuma ser bem movimentado aos finais de semana por moradores da região",
+        link:"https://www.viamao.rs.gov.br/portal/0/galeria-de-fotos/151/lago-taruma/",
+        tags:" lazer lago perto do centro de viamão chimarrão ",
+     },
+     {
+        titulo:" Saint’Hilaire",
+        descricao:"Parque Natural Municipal Saint’Hilaire, é uma Unidade de Conservação da Natureza, categoria Parque, pertencente ao município brasileiro de Porto Alegre. Sua área está dividida entre os municípios de Viamão (a maior parte) e Porto Alegre. O parque é aberto para visitação. A entrada está em Viamão, na avenida Senador Salgado Filho, 2785, bairro Vera Cruz.[1]",
+        link:"https://pt.wikipedia.org/wiki/Parque_Natural_Municipal_Saint%27Hilaire",
+        tags:"parque chimarrao barragem conservação ",
+     },
+    
+     {
+        titulo:"Parque Estadual de Itapuã",
+        descricao:"O Parque Estadual de Itapuã destina-se à proteção das belezas e recursos naturais, em especial a flora e a fauna, e à proteção dos sítios de valor histórico e arqueológico existentes na região onde ocorre o encontro das águas do Lago Guaíba e da Laguna dos Patos, num conjunto de ambientes de morros, praias, dunas, lagoas e banhados em plena região metropolitana de Porto Alegre. Dentre as espécies da fauna preservada pelo Parque salienta-se o bugio-ruivo (Aloutatta guariba), ameaçado de extinção, a lontra (Lontra longicaudis), o gato-maracajá (Leopardus wiedii), bem como aves migratórias como o maçarico-acanelado (Tryngites subruficollis) e o suiriri (Tyrannus melancholicus).",
+        link:"https://www.sema.rs.gov.br/parque-estadual-de-itapua/",
+        tags:"proteção ambiente flora fauna beleza",
+     },
+    
+];
